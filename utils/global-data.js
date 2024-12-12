247430542d06bd45cf15837dc7875a7c05f1ba79
王俊杰 <74604862+xiaowang27@users.noreply.github.com>
2024-12-12 10:25:43 +0800

    test
     测试

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '鲨鱼辣椒侦探';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '人生笔记';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '永远相信美好的事情即将发生';
 
   return {
     name,
